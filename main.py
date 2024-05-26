097e5bcc6eda62ff54d52a69ade5dd9ee0efa47a
ShutSV <132217445+ShutSV@users.noreply.github.com>
2024-05-26 11:56:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ client = openai.OpenAI()
 
 def text_generate(msg):
     messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "system", "content": "You are a useful assistant, you answer in Belarusian."},
         {"role": "user", "content": msg},
     ]
     return client.chat.completions.create(
